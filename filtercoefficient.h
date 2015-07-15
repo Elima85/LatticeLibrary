f84cb3f522803185d0766b3fbd309deeb142f238
Elisabeth Linnér <e.scholdlinner@gmail.com>
2015-07-15 15:36:35 +0200

    Fixed table problem.
    Former-commit-id: e22c6d5aab2398db6774968d9a157256cf4b40eb

--- filtercoefficient.h
+++ filtercoefficient.h
@@ -4,12 +4,12 @@
 namespace LatticeLib {
 
 /**
- * Filter coefficient for template filters.
+ * Coefficient for template filters.
  *
  * Member 		| Comment
  * --------		| --------
  * index		| Position index of the element in a neighborhood. The midpoint element has index -1.
- * coefficient	| Filter coefficient for the specified element.
+ * coefficient	| %Filter coefficient for the specified element.
  */
     template<class S>
     class FilterCoefficient {
@@ -18,7 +18,7 @@ namespace LatticeLib {
         /** Position index of the element in a neighborhood. The midpoint element has index -1. */
         int positionIndex;
 
-        /** Filter coefficient for the specified element. */
+        /** %Filter coefficient for the specified element. */
         S coefficient;
 
     public:
@@ -33,7 +33,7 @@ namespace LatticeLib {
         /**
          * Constructor for FilterCoefficient objects.
          *
-		 * Parameter	| in/out	| Comment
+         * Parameter	| in/out	| Comment
 		 * :---------  	| :-------	| :--------
          * i            | INPUT     | Position index.
          * c            | INPUT     | Corresponding coefficient value.
@@ -60,8 +60,8 @@ namespace LatticeLib {
         /**
          * Sets positionIndex to the input value.
          *
-		 * Parameter	| in/out	| Comment
-		 * :---------  	| :-------	| :--------
+         * Parameter    | in/out	| Comment
+		 * :----------  | :-------  | :--------
          * i            | INPUT     | New position index.
          */
         void setPositionIndex(int i) {
@@ -71,8 +71,8 @@ namespace LatticeLib {
         /**
          * Sets coefficient to the input value.
          *
-		 * Parameter	| in/out	| Comment
-		 * :---------  	| :-------	| :--------
+         * Parameter    | in/out    | Comment
+		 * :----------  | :-------  | :--------
          * c            | INPUT     | New coefficient value.
          */
         void setCoefficient(S c) {
@@ -82,8 +82,8 @@ namespace LatticeLib {
         /**
          * Sets positionIndex and coefficient to the input values.
          *
-		 * Parameter	| in/out	| Comment
-		 * :---------  	| :-------	| :--------
+         * Parameter	| in/out	| Comment
+		 * :----------  | :-------  | :--------
          * i            | INPUT     | New position index.
          * c            | INPUT     | New coefficient value.
          */
